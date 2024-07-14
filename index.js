462ebb30c9799acd2a90908835bd3810ea1da6f6
DineshKuddana <dineshkuddana@gmail.com>
2024-07-14 12:22:05 +0530

    whatsapp_feature

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ function sendwhatsapp() {
     var first = document.querySelector('.first').value;
     var last = document.querySelector('.last').value;
     var email = document.querySelector('.email').value;
-
     var number = document.querySelector('.number').value;
-   
 
     if (first && last && email && number) {
-        var phonenumber = ' 8178237828';        
+        var phonenumber = '+917801011369';
         var city = document.querySelector('.city').value || "Not provided";
 
-        var url = 'https://wa.me/' + phonenumber + "?text="
-        + "*Name :* " + first + last + "%0a"
-        + "*Email :* " + email + "%0a"
-        + "*Contact:* " + number + "%0a"
-        + "*City :* " + city + "%0a";
+        var message = 
+            "Name: " + first + " " + last + "\n" +
+            "Email: " + email + "\n" +
+            "Contact: " + number + "\n" +
+            "City: " + city ;
 
-        window.open(url, '_blank').focus();
+        var encodedMessage = encodeURIComponent(message);
+        var url = 'https://wa.me/' + phonenumber + '?text=' + encodedMessage;
+
+        // Open WhatsApp in the same window/tab with the pre-filled message
+        window.location.href = url;
     } else {
         alert("Please fill in all required fields.");
     }
-    
-}  
+}
